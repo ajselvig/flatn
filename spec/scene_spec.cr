7704debaf8c5feb3204f5d64a9b562a4384fa095
Andy Selvig <ajselvig@gmail.com>
2018-03-31 09:39:50 -0500

    bottle rendering

--- spec/scene_spec.cr
+++ spec/scene_spec.cr
@@ -3,7 +3,7 @@ include Flatn
 
 describe Scene do
 
-  it "makes pretty pictures" do 
+  it "renders a sphere" do 
     width = 600
     height = 600
 
@@ -20,8 +20,25 @@ describe Scene do
     scene.filters << WireframeFilter.new(StrokeStyle.new("#000000", width=0.5))
     
     scene.render "spec/output/sphere_flat_wireframe"
+  end
 
+  it "renders a bottle" do 
+    width = 600
+    height = 600
 
+    scene = Scene.new
+    scene.camera.perspective(10.0, 10.0, 10.0, 200.0)
+      .translate(0.0, -45.0, 110.0)
+      # .rotate_x(-45.0)
+
+    scene.models << STLLoader.new.from_file("spec/input/bottle.stl")
+
+    scene.renderers << SVGRenderer.new(width, height)
+
+    scene.filters << FlatFilter.new(FillStyle.new("#00ffff"))
+    scene.filters << WireframeFilter.new(StrokeStyle.new("#000000", width=0.5))
+    
+    scene.render "spec/output/bottle_flat_wireframe"
   end
 
 
